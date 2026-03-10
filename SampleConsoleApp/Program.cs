b91369c2a12b612dfd6b31c59d8d14a1c4174e7b
Bharathi Kannan Natarajan <natarajan.bharathikannan@in.bosch.com>
2026-03-10 12:46:04 +0530

    Add project files.

---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SampleConsoleApp
+{
+    public class Master
+    {
+
+        public static int GetRandomNumber()
+        {
+            return new Random().Next(1, 100);
+        }
+
+        public static void Main()
+        {
+            int number = GetRandomNumber();
+            if (number > 10)
+            {
+                Console.WriteLine("number > 10");
+            }
+            else
+            {
+                Console.WriteLine("number < 10");
+            }
+        }
+    }
+}
---
+++ SampleConsoleApp/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SampleConsoleApp
+{
+    public class Utils
+    {
+        public Utils()
+        {
+        }
+
+        public static int Fact(int number)
+        {
+            if (number == 0) { return 1; }
+            return (number * Fact(number - 1));
+        }
+    }
+}
+
