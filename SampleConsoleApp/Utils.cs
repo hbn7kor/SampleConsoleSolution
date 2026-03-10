26e6d368b196560856e43ab673ba0138dcca824e
Bharathi Kannan Natarajan <natarajan.bharathikannan@in.bosch.com>
2026-03-10 15:12:06 +0530

    Adding a Prime number function

--- SampleConsoleApp/Utils.cs
+++ SampleConsoleApp/Utils.cs
@@ -13,6 +13,16 @@ namespace SampleConsoleApp
             if (number == 0) { return 1; }
             return (number * Fact(number - 1));
         }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) { return false; }
+            for (int index = 2; index <= (int)Math.Sqrt(number); index++)
+            {
+                if (number % index == 0) { return false; }
+            }
+            return true;
+        }
     }
 }
 
