1dd28f59e4753321afc2d8ea685baa5f35f2ac9e
Bharathi Kannan Natarajan <natarajan.bharathikannan@in.bosch.com>
2026-03-11 10:59:49 +0530

    Adding some more commits to the repository

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -2,9 +2,23 @@
 {
     public class Program
     {
+        public static int Fact(int number)
+        {
+            if (number == 0) { return 1; }
+            return number * Fact(number - 1);
+        }
+
         public static void Main()
         {
+            Console.WriteLine(Fact(10));
             Console.WriteLine("The quick brown fox jumps over the lazy dog");
         }
+
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        
     }
 }
